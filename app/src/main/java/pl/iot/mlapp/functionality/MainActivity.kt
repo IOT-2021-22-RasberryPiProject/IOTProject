d5d1e6d6cfe7b9d1b301eaac55897ce6c1a9963e
Paweł Klecha <ThePablo1720@gmail.com>
2022-01-07 23:11:36 +0100

    Initial commit

---
+++ app/src/main/java/pl/iot/mlapp/functionality/MainActivity.kt
@@ -0,0 +1,59 @@
+package pl.iot.mlapp.functionality
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import pl.iot.mlapp.R
+import pl.iot.mlapp.databinding.ActivityMainBinding
+import pl.iot.mlapp.di.appModule
+import pl.iot.mlapp.functionality.camera.CameraFragment
+import pl.iot.mlapp.functionality.notifications.NotificationsFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupKoin()
+        setupViews()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@MainActivity)
+            modules(appModule)
+        }
+    }
+
+    private fun setupViews() {
+        setSupportActionBar(binding.toolbar)
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.pageCamera -> {
+                    supportFragmentManager.commit {
+                        replace<CameraFragment>(R.id.fragmentContainer)
+                    }
+                    true
+                }
+                R.id.pageNotifications -> {
+                    supportFragmentManager.commit {
+                        replace<NotificationsFragment>(R.id.fragmentContainer)
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
